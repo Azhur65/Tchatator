8d605113052e73dae195d03960fdeac7ace36542
Azhur65 <jan.donal.cox@gmail.com>
2025-02-01 22:37:20 +0100

    added send message functions

--- serveur.c
+++ serveur.c
@@ -6,8 +6,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <libpq-fe.h>
+#include <stdbool.h>
+#include <time.h>
 
 #define BUFFER_SIZE 1024
+
+const int MAX_MSG_SIZE = 1000;
+
+//sends the message msg to the user specified by dest
+char* send_message(PGconn* dbConn, int src, int dest, char* msg);
+
+//returns a query to send a message
+char* insert_message_query(int src, int dest, char* msg);
+
+//checks if the user that sent the request is blocked by the user specified by dest
+bool is_blocked(PGconn* dbConn, int src, int dest);
 
 int main() {
     int sock, ret, cnx;
@@ -15,6 +29,15 @@ int main() {
     struct sockaddr_in conn_addr, addr;
     char buffer[BUFFER_SIZE];
     size = sizeof(conn_addr);
+
+    // Connection a la bdd
+    const char CONN_INFO[] = "host=srfc.ventsdouest.dev port=5432 dbname=sae user=sae password=escapade-Venait-s1gner connect_timeout=30";
+    PGconn* dbConn = PQconnectdb(CONN_INFO);
+    if (PQstatus(dbConn) != CONNECTION_OK) {
+        printf("Error with database connection : %s\n", PQerrorMessage(dbConn));
+        PQfinish(dbConn);
+        exit(1);
+    }
 
     // Créer un socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -96,6 +119,75 @@ int main() {
     // Fermer la connexion et le socket
     close(cnx);
     close(sock);
+    PQfinish(dbConn);
 
-    return 0;
+    return EXIT_SUCCESS;
+}
+
+char* send_message(PGconn* dbConn, int src, int dest, char* msg) {
+    char *ret;
+    //return codes and messages
+    const char OK[] = "200/OK : Message envoyé";
+    const char ERROR[] = "400/ERROR : Le message n'a pas été envoyé";
+    const char BLOCKED[] = "401/BLOCKED : Le destinataire n'autorise pas les messages de votre part";
+    const char LONG[] = "402/LONG : Le message est trop long";
+    const char UNDEFINED[] = "404/UNDEFINED : L'identifiant du client est incorrect";
+    const char SPAM[] = "405/SPAM : Le message n'a pas été envoyé car trop de messages ont été envoyés en peu de temps";
+
+    //checks
+    if (is_blocked(dbConn, src, dest)) {
+        ret = malloc(sizeof(BLOCKED));
+        strcpy(ret, BLOCKED);
+        return ret;
+    }
+
+    if (strlen(msg) >= MAX_MSG_SIZE) {
+        ret = malloc(sizeof(LONG));
+        strcpy(ret, LONG);
+        return ret;
+    }
+
+    //send to db
+    PGresult* res = PQexec(dbConn, insert_message_query(src, dest, msg));
+    char* error = PQresultErrorMessage(res);
+    if (strcmp(error, "") != 0) {
+        if (strstr(error, "message_fk_2") != NULL) {
+            ret = malloc(sizeof(UNDEFINED));
+            strcpy(ret, UNDEFINED);
+            return ret;
+        } else {
+            ret = malloc(sizeof(ERROR));
+            strcpy(ret, ERROR);
+            return ret;
+        }
+    }
+
+    PQclear(res);
+    ret = malloc(sizeof(OK));
+    strcpy(ret, OK);
+    return ret;
+}
+
+char* insert_message_query(int src, int dest, char* msg) {
+    char* ret;
+    char temp[2048];
+
+    char queryBase[] = "INSERT INTO chatator.message(message, envoyeur, receveur, date, modifie) VALUES(";
+    sprintf(temp, "%s, %d, %d, %li, false)", msg, src, dest, (long) time(NULL));
+    ret = malloc(sizeof(queryBase) + sizeof(char) * (strlen(temp) + 1));
+    ret = strcat(queryBase, temp);
+
+    return ret;
+}
+
+bool is_blocked(PGconn* dbConn, int src, int dest) {
+    bool blocked = false;
+    char query[4096];
+    
+    sprintf(query, "SELECT date_deblocage FROM chatator.conversation WHERE (client_id_1 = %d AND client_id_2 = %d) OR (client_id_2 = %d AND client_id_1 = %d)", src, dest, src, dest);
+    PGresult* res = PQexec(dbConn, query);
+    if (strstr(PQgetvalue(res, 0, 0), "true") != NULL) blocked = true;
+
+
+    return blocked;
 }
